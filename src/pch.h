0ff56086ae4fa7dca1230726012e92a81b6e04ec
alban <albanguyon@yahoo.fr>
2024-12-03 18:54:29 -0500

    updated pch to include all includes

--- src/pch.h
+++ src/pch.h
@@ -1,43 +1,7 @@
 #pragma once
-// pch.h: This is a precompiled header file.
-// Files listed below are compiled only once, improving build performance for
-// future builds. This also affects IntelliSense performance, including code
-// completion and many code browsing features. However, files listed here are
-// ALL re-compiled if any one of them is updated between builds. Do not add
-// files here that you will be updating frequently as this negates the
-// performance advantage.
-
 #define _CRT_SECURE_NO_WARNINGS
 
-// add headers that you want to pre-compile here
-#include <algorithm>
-#include <array>
-#include <atomic>
-#include <chrono>
-#include <cmath>
-#include <condition_variable>
-#include <ctime>
-#include <filesystem>
-#include <fstream>
-#include <functional>
-#include <future>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <memory>
-#include <mutex>
-#include <optional>
-#include <random>
-#include <set>
-#include <sstream>
-#include <stdexcept>
-#include <string>
-#include <thread>
-#include <type_traits>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
